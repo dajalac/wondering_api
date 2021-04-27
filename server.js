29a05a821e2e52092767fb8e2ddbf914342a30a3
danielle ajala <danielleajala@gmail.com>
2021-04-27 06:36:30 -0500

    database connection created

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express= require ('express');
 const {response} = require('express');
-const cors = require('cors')
+const knex = require('knex')
+const cors = require('cors');
+
 const dotenv = require('dotenv');
 
 dotenv.config(); //to grab the env variables
+
+// conecting to the database
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1', // indicates where the db is. For now it is in my computer 
+      user : 'postgres', //user of the db. just give \d to find out
+      password : ' ',
+      database : 'wondering'
+    }
+  });
 
 
 const app = express();
 app.use(express.urlencoded({extented:false}));
 app.use(express.json()); 
 app.use(cors());
+
+
+
 
 app.get('/', (req, res)=>{
     res.json('this is working')
