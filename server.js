fecffcc6fbfb5a8d023e785cf44a3e4e74a3452d
danielle ajala <danielleajala@gmail.com>
2021-04-28 06:18:35 -0500

    calling handlers in server file

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express= require ('express');
 const {response} = require('express');
-const knex = require('knex')
+const knex = require('knex');
+const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-
 const dotenv = require('dotenv');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
 
 dotenv.config(); //to grab the env variables
 
@@ -31,24 +36,16 @@ app.get('/', (req, res)=>{
     res.json('this is working')
 })
 
-app.post('/signin', (req, res)=>{
-    res.json ('signing')
-})
+app.post('/signin', (req, res) => {signin.signinHandler(req, res, db, bcrypt)})
 
-app.post('/register',(req, res)=>{
-    const {name, email} = req.body;
+app.post('/register', (req, res) => {register.registerHandler(req, res, db, bcrypt)})
 
-    db('users').insert({
-        email:email,
-        name:name,
-        joined: new Date()
-    })
-    .returning('*')
-    .then(response =>{
-        res.json(response[0])
-    })
-    .catch(err => res.status(400).json('unable to register'))
-})
+// : means that you can put watever you want. In our case we are putting the user id
+app.get('/profile/:id', (req, res) => {profile.profileHandler(req, res, db)})
+
+app.put('/image', (req, res) => {image.imageHandler(req, res, db)})
+app.post('/imageurl', (req, res) => {image.apiCallHandler(req, res)})
+
 
 app.listen(process.env.PORT, ()=>{
     console.log(`app is up running ${process.env.PORT}`)
