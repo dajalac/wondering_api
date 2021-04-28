7a3523e3edf26dae619789b6f9f0cde989d31667
danielle ajala <danielleajala@gmail.com>
2021-04-28 05:50:29 -0500

    profile controller created

---
+++ controllers/profile.js
@@ -0,0 +1,15 @@
+const profileHandler = (req, res, db) => {
+    const { id } = req.params;
+
+    db.select('*').from('users').where({ id: id })
+        .then(user => {
+            user.length 
+                ? res.json(user[0])
+                : res.status(400).json('user not found')
+        })
+        .catch(err => res.status(400).json('error getting user'))
+}
+
+module.exports ={
+    profileHandler: profileHandler
+}
