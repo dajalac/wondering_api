9a209c650ded3929778617a8793def60c9db05e3
danielle ajala <danielleajala@gmail.com>
2021-04-30 06:22:20 -0500

    trying to get user ranking

--- controllers/image.js
+++ controllers/image.js
@@ -1,4 +1,5 @@
 const Clarifai = require('clarifai');
+//const { json } = require('express');
 const dotenv = require('dotenv');
 
 dotenv.config(); //to grab the env variables
@@ -9,6 +10,7 @@ const app = new Clarifai.App({
 })
 
 const apiCallHandler =(req, res)=>{
+   
     app.models
         .predict(Clarifai.FACE_DETECT_MODEL,req.body.input)
         .then(data =>{
@@ -18,15 +20,38 @@ const apiCallHandler =(req, res)=>{
 }
 
 const imageHandler =(req, res,db)=>{
-    const{id} = req.body;
+    const{id, numberOfFace} = req.body;
 
+    // increment the number of faces detected
+     const userNumberOfFaces = db('users').where('id', '=', id)
+        .increment('number_faces', numberOfFace.lenght)
+        .returning('number_faces')
+        .then(value =>{
+             console.log('value', value)
+        })
+
+    console.log('number of faces',numberOfFace[0])
+
+    // ranking 
+        db('users').count('id as ranking')
+        .where('number_faces'> userNumberOfFaces)
+        .returning('ranking')
+        .then(ranking =>{
+            console.log(Object.values(ranking[0])+1)
+            console.log(ranking[0])
+            res.json(ranking)
+        })
+        .catch(err => res.status(400).json('unable to get ranking'))
+       
+
+    /*
     db('users').where('id', '=', id)
-        .increment('number_faces', 1)
+        .increment('number_faces', numberOfFaces)
         .returning('number_faces')
         .then(entries => {
             res.json(entries[0])
         })
-        .catch(err => res.status(400).json('unable to get entries'))
+        .catch(err => res.status(400).json('unable to get entries'))*/
 }
 
 module.exports ={
